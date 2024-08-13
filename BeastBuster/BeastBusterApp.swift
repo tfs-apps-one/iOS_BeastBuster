b2ca42e510d7047520f0e6fb0efeaff31be64018
tfs-apps-one <furu12080205@gmail.com>
2024-08-13 11:17:03 +0900

    Initial Commit

---
+++ BeastBuster/BeastBusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeastBusterApp.swift
+//  BeastBuster
+//
+//  Created by 古川貴史 on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct BeastBusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeastBuster/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BeastBuster
+//
+//  Created by 古川貴史 on 2024/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
